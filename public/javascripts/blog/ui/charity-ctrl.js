75d2ce5d218adc771c0ff04a925a397198c98532
rory payne <rorypayne@rorys-MacBook.local>
2016-06-08 00:17:58 +0100

    UI updates

--- public/javascripts/blog/ui/charity-ctrl.js
+++ public/javascripts/blog/ui/charity-ctrl.js
@@ -2,14 +2,9 @@ blogUiModule.controller("CharityCtrl", [
     '$scope',
     'fundraiserService',
     function($scope, fundraiserService) {
-
-//        fundraiserService.getFundraiserSummary()
-//            .success(function(data, status, header, config) {
-//                $scope.fundraiserSummary = data;
-//            })
-//            .error(function(data, status, header, config) {
-//                //TODO:
-//                console.log(data);
-//            });
         $scope.virginGivingUrl = "http://uk.virginmoneygiving.com/fundraiser-web/fundraiser/showFundraiserProfilePage.action?userUrl=RoryandJeremy&isTeam=true";
+        $scope.coolEarthUrl = "https://www.coolearth.org/campaigns/9019/rory-jeremys-mongol-rally-on-motorbikes-6/";
+        $scope.bhfUrl = "https://www.bhf.org.uk/about-us";
+        $scope.dementiaUrl = "https://www.dementiauk.org/";
+        $scope.mindUrl = "www.mind.org.uk";
 }]);
